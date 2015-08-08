8da76b617c31ea684a3accd6c1dbbe12ff394048
bssthu <bssthu@users.noreply.github.com>
2015-08-09 00:46:57 +0800

    generate QR code in game

--- quest_text/quest.lua
+++ quest_text/quest.lua
@@ -15,45 +15,46 @@ local wnd_attr = {
 }
 
 local digit_attr = {
-    width = 2,
-    height = 2,
+    width = 4,
+    height = 4,
+    number = 8192,
 }
 
 --** local functions **--
 
-local function getHash(str) -- BKDRHash
-    local seed = 131
-    local hash = 0
-    for i = 1, #str do
-        local ch = string.byte(str, i)
-        hash = hash * seed + tonumber(ch)
-        hash = hash % 0x80000000
-    end
-    return hash
-end
-
 local function ShowQrcode(tab)
     -- hide all digits
-    for i = 1, 2048 do
+    for i = 1, digit_attr.number do
         textures[i]:SetHidden(true)
     end
+
+    -- check
+    if #tab > digit_attr.number then
+        d("|ceee6666so_zh: text too long|r")
+        return
+    end
+
     -- place white digits
     local row = 0
-    local col = 0
-    --for i = 1, #tab do
-    --    textures[i]:SetAnchor(TOPLEFT, ESOZH.zhWnd, TOPLEFT,
-    --        wnd_attr.hmargin + col * digit_attr.width, wnd_attr.vmargin + row * digit_attr.height)
-    --    textures[i]:SetHidden(false)
-    --end
-    textures[1]:SetAnchor(TOPLEFT, ESOZH.zhWnd, TOPLEFT,
-        wnd_attr.hmargin + col * digit_attr.width, wnd_attr.vmargin + row * digit_attr.height)
-    textures[1]:SetHidden(false)
+    local index = 0
+    for row = 1, #tab do
+        local rowArray = tab[row]
+        for col = 1, #rowArray do
+            if rowArray[col] > 0 then
+                index = index + 1
+                textures[index]:SetAnchor(TOPLEFT, ESOZH.zhWnd, TOPLEFT,
+                        wnd_attr.hmargin + col * digit_attr.width, wnd_attr.vmargin + row * digit_attr.height)
+                textures[index]:SetHidden(false)
+            end
+        end
+    end
 end
 
 local function ShowTextQrcode(text)
-    local hash = getHash(text)
-    tab = qrencode.qrcode(text)
-    ShowQrcode(tab)
+    local ok, tab = qrencode.qrcode(text)
+    if ok then
+        ShowQrcode(tab)
+    end
 end
 
 --** public functions **--
@@ -64,7 +65,7 @@ ZO_InteractWindowTargetAreaBodyText.SetText = function (self, bodyText)
 end
 
 function ESOZH.QUEST:Initialize()
-    for i = 1, 2048 do
+    for i = 1, digit_attr.number do
         textures[i] = WINDOW_MANAGER:CreateControl("texture"..tostring(i), ESOZH.zhWnd, CT_TEXTURE)
         textures[i]:SetDimensions(digit_attr.width, digit_attr.height)
         textures[i]:SetHidden(true)
