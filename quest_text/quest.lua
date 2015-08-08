c09abdff1dfa4995c52461475052c9b3e9c036b5
bssthu <bssthu@users.noreply.github.com>
2015-08-08 23:35:30 +0800

    give up ingame translate
    will use qr code to send text out.

--- quest_text/quest.lua
+++ quest_text/quest.lua
@@ -7,13 +7,6 @@ local Origin_InteractWindow_SetText= ZO_InteractWindowTargetAreaBodyText.SetText
 
 local textures = {}
 
-local char_attr = {
-    width = 32,
-    height = 32,
-    hmargin = 0,
-    vmargin = 0,
-}
-
 local wnd_attr = {
     width = 250,
     height = 300,
@@ -34,65 +27,39 @@ local function getHash(str) -- BKDRHash
     return hash
 end
 
+local function ShowTextQrcode(text)
+    local hash = getHash(text)
+    -- TODO
+end
+
+function ShowQrcode(tab)
+    -- hide all digits
+    for i = 1, 2048 do
+        textures[i]:SetHidden(true)
+    end
+    -- place white digits
+    --row = 0
+    --col = 0
+    --for i = 1, #tab do
+    --    textures[i]:SetAnchor(TOPLEFT, ESOZH.zhWnd, TOPLEFT,
+    --        wnd_attr.hmargin - char_attr.hmargin + col * (char_attr.width + char_attr.hmargin),
+    --        wnd_attr.vmargin - char_attr.vmargin + row * (char_attr.height + char_attr.vmargin))
+    --    textures[i]:SetHidden(false)
+    --end
+end
+
 --** public functions **--
 
 ZO_InteractWindowTargetAreaBodyText.SetText = function (self, bodyText)
     Origin_InteractWindow_SetText(self, bodyText)
-    ESOZH.QUEST:TranslateAndShowText(bodyText)
-end
-
-function ESOZH.QUEST:TranslateAndShowText(text)
-    local hash = getHash(text)
-    local ids = ESOZH.QUEST.hashTbl[tonumber(hash)]
-    d(hash)
-    thehash = hash
-    d(ids)
-    if ids == nil then
-        return
-    end
-    if #ids > 1 then
-        for id in ids do
-            if text == ESOZH.QUEST.enTbl[id] then
-                local texts = ESOZH.QUEST.zhTbl[id]
-                ESOZH.QUEST:ShowText(texts)
-                break
-            end
-        end
-    else
-        local texts = ESOZH.QUEST.zhTbl[ids[0]]
-        ESOZH.QUEST:ShowText(texts)
-    end
+    ShowTextQrcode(bodyText)
 end
 
 function ESOZH.QUEST:Initialize()
     for i = 1, 2048 do
         textures[i] = WINDOW_MANAGER:CreateControl("texture"..tostring(i), ESOZH.zhWnd, CT_TEXTURE)
-        textures[i]:SetDimensions(char_attr.width, char_attr.height)
+        textures[i]:SetDimensions(1, 1)
         textures[i]:SetHidden(true)
-    end
-end
-
-function ESOZH.QUEST:ShowText(textTbl)
-    -- hide all chars
-    for i = 1, 2048 do
-        textures[i]:SetHidden(true)
-    end
-    -- place chars
-    charsInARow = (wnd_attr.width - 2 * wnd_attr.hmargin + char_attr.hmargin) / (char_attr.width + char_attr.hmargin)
-    row = 0
-    col = 0
-    d(textTbl)
-    for i = 1, #textTbl do
-        d(tostring(textTbl[i]))
-        textures[i]:SetAnchor(TOPLEFT, ESOZH.zhWnd, TOPLEFT,
-                wnd_attr.hmargin - char_attr.hmargin + col * (char_attr.width + char_attr.hmargin),
-                wnd_attr.vmargin - char_attr.vmargin + row * (char_attr.height + char_attr.vmargin))
-        textures[i]:SetTexture('eso_zh/Fonts_dds/'..tostring(textTbl[i])..'.dds')
-        textures[i]:SetHidden(false)
-        col = col + 1
-        if row > charsInARow then
-            row = 0
-            col = row + 1
-        end
+        textures[i]:SetTexture('eso_zh/texture/white.dds')
     end
 end
